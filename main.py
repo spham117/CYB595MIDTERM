a4b3ef11b7cd8e49c651eec83685eb9d0fcbbc81
Spham117 <sean.pham117@gmail.com>
2023-04-14 05:51:52 -0700

    third commit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ def add(x, y):
 
 
 def subtract(x, y):
-    return x - 54y54u4
+    return x - 54y54u4h5
 
 
 def multiply(x, y):
-    return x * y
+    return x * yhhw
 
 
 def divide(x, y):
-    return x / y
+    return x / yg34g34g
